bd4c34445091ea5e3e9fdac0455a7ebbecc7354d
Juan David Rendon Berrio <juan.rendon13@udea.edu.co>
2022-01-31 11:16:35 -0500

    En la SplashActivity se crea el lateinit var splashBinding y una funcion llamada goToMainActivity para decirle a la actividad que despues de mostrar la pantalla de splash por dos segundos, nos muestre la pantalla de inicio de la app.
    Se crea en su respectivo xml un imageView centrado en la pantalla con
    nuestro logo de la App.
    
    Se modifica el Manifiest para configurarlo apropiadamente con los
    parametros para el splash, con el stilo creado en themes para que no se
    muestre la topBar.

--- app/src/main/java/com/juancho1037/umadefodds/splash/SplashActivity.kt
+++ app/src/main/java/com/juancho1037/umadefodds/splash/SplashActivity.kt
@@ -1,12 +1,34 @@
 package com.juancho1037.umadefodds.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.juancho1037.umadefodds.MainActivity
 import com.juancho1037.umadefodds.R
+import com.juancho1037.umadefodds.databinding.ActivitySplashBinding
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashBinding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        val timer = Timer()
+        timer.schedule(
+            timerTask {
+                goToMainActivity()
+            }, 2000
+        )
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
     }
 }
