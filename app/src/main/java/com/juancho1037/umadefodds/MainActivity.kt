5276f7a2b0bed79bd83a3f66e4b020c7236f1179
Juan David Rendon Berrio <juan.rendon13@udea.edu.co>
2022-01-31 10:46:24 -0500

    Creacion del proyecto U Made Foods

---
+++ app/src/main/java/com/juancho1037/umadefodds/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.juancho1037.umadefodds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
