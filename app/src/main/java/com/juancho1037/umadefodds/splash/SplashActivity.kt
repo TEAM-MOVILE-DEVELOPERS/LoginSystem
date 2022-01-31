da5cab0fccbf242527b08068f4cfe6ee61c8cc1a
Juan David Rendon Berrio <juan.rendon13@udea.edu.co>
2022-01-31 10:51:17 -0500

    Se agregan las dependencias en el gradle del proyecto. Se crea un nuevo package dentro de la carpeta de U Made Foods para el splash, y se crea una nueva actividad llamada splashActivity.
    Se eliminan los mipmap por defecto que crea el proyecto para agregar el
    nuevo icono de la App.
    Se carga la nueva image asset para el icono.
    Se carga un nuevo drawable para la imagen que tendra el splash cuando
    inicie nuestra app.

---
+++ app/src/main/java/com/juancho1037/umadefodds/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.juancho1037.umadefodds.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juancho1037.umadefodds.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
