dcc9b5ff0861991ea635a9f8d65d762a29cce954
Juan David Rendon Berrio <juan.rendon13@udea.edu.co>
2022-02-01 00:07:02 -0500

    Se crea el diseño del registro que va a tener la App. Se crean los intent, que son los encargados de administrar el envio de informacion entre actividades. Se crea un menu de OverFlow con un item para cerrar sesion.

--- app/src/main/java/com/juancho1037/umadefodds/MainActivity.kt
+++ app/src/main/java/com/juancho1037/umadefodds/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.juancho1037.umadefodds
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.juancho1037.umadefodds.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,4 +16,23 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_sign_out -> goToLoginActivity()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class,java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
 }
